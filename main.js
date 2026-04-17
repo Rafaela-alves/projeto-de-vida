5feb090dc7de22bfcdcf072eb82278f4b824eac6
Rafaela-alves <rafaela.alves.silva23@escola.pr.gov.br>
2026-04-16 21:23:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00"); 
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00"); 
-const tempoObjetivo3 = new Date("2025-12-30T00:00:00"); 
-const tempoObjetivo4 = new Date("2025-10-05T00:00:00"); 
+const tempoObjetivo1 = new Date("2026-10-05T15:30:00"); // 5 de outubro de 2026 às 15:30:00
+const tempoObjetivo2 = new Date("2026-12-05T08:00:00"); // 5 de dezembro de 2026 às 08:00:00
+const tempoObjetivo3 = new Date("2026-12-30T23:59:59"); // 30 de dezembro de 2026 às 23:59:59
+const tempoObjetivo4 = new Date("2026-10-05T00:00:00"); // 5 de outubro de 2026 à meia-noite
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
